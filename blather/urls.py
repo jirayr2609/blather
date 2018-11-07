2ebadd3cbb1eff83b2f69b1758699e19a03e29fa
Zhirayr Abrahamyan <zabraha000@citymail.cuny.edu>
2018-11-06 19:08:39 -0500

    more views

--- blather/urls.py
+++ blather/urls.py
@@ -18,6 +18,7 @@ from blat import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home, name = "homepage"),
-
+    #url(r'^$', views.home, name = "homepage"),
+    url(r'^$',views.IndexView.as_view(),name = "homepage"),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name = 'detail'), 
 ]
